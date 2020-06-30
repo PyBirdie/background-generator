d69d0b42de6fa08ac17c1bfe02fd31e82da6793b
Nazish Bhatti (nbhatti) <nbhatti@cisco.com>
2020-06-30 08:34:19 -0400

    removing some code for list functions

--- smarty_pants.py
+++ smarty_pants.py
@@ -1,33 +1,5 @@
 import re
 
-def list_exercise(list):
-    # Check that list is only int/real numbers and return error if otherwise
-    for number in list:
-        if type(number) != int:
-            print("NO FUN: Please provide a list with only integer numbers.")
-            return
-        
-    print("median = " + str(list[int(len(list)/2)]))
-    print("average = " + str(sum(list)/len(list)))
-    print("sum = " + str(sum(list)))
-    
-    descending_sorted = sorted(list)
-    descending_sorted.sort(reverse=True)
-    print("descending_sorted = " + str(descending_sorted))
-    
-    both_1_and_5 = []
-    either_1_or_5 = []
-    for number in list:
-        if (re.search("[15]",str(number))):
-            either_1_or_5.append(number)
-        if (re.search("1",str(number))) and (re.search("5",str(number))):
-            both_1_and_5.append(number)
-    print("both_1_and_5 = " + str(both_1_and_5))
-    print("either_1_or_5 = " + str(either_1_or_5))
-    
-    return
-    
-
 ###Smarty Pants
 
 
